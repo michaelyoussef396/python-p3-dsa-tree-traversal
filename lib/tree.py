db23df0d0416d49b54c6b131519c4f2b4d0073f0
michaelyoussef396 <michaelyoussef396@gmail.com>
2024-05-14 12:19:26 +1000

    finished tests

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,18 @@
 class Tree:
-  def __init__(self, root = None):
-    self.root = root
+    def __init__(self, root=None):
+        self.root = root
 
-  def get_element_by_id(self, id):
-    pass
+    def get_element_by_id(self, id):
+        return self._get_element_by_id_recursive(self.root, id)
+
+    def _get_element_by_id_recursive(self, node, id):
+        if node is None:
+            return None
+        if node.get('id') == id:
+            return node
+        if 'children' in node:
+            for child in node['children']:
+                result = self._get_element_by_id_recursive(child, id)
+                if result is not None:
+                    return result
+        return None
